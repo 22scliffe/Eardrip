8475b473e8a070eb8fb2ccb49f1172e9b61276b7
Sam Cliffe <sam.j.cliffe@outlook.com>
2021-12-11 14:40:13 +0000

    1st commit

---
+++ PYTHON/Log_In_Function.py
@@ -0,0 +1,37 @@
+from Homepage import *
+username = ("sam cliffe")
+password = ("123")
+LoggedIn = False
+
+def login():
+    validusername = False
+    validpassword = False
+    attempts = int(3)
+    print("Welcome to the login page")
+
+    #checks validity of username
+    while validusername == False:
+        userinput = input ("please enter your username: ")
+        if userinput == username:
+            validusername = True
+        else:
+            print("Invalid username, please try again")
+            validusername = False
+
+    #checks validity of password + gives the user 3 attempts to get the password correct
+    while validpassword == False:
+        userinput = input ("please enter your password: ")
+        if userinput == password:
+            validpassword = True
+            print("welcome, "+username)
+            LoggedIn = True
+            homepage()
+        else:
+            print("Invalid password")
+            attempts = (attempts - 1)
+            if attempts == (0):
+                print("you have 0 attempts left")
+                quit()
+            else:
+                print("you have",attempts,"attempts left, please try again")
+            validpassword = False
